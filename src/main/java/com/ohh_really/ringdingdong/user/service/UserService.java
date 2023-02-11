6abd575f9247d23f5e7628840c95ecd73113b66e
Namju Kim <cmsong111@naver.com>
2023-02-12 06:28:28 +0900

    2 feat 회원 관리 기능 (#3)
    * feat : User 엔티티, 회원가입 폼 제작
    
    * feat: 구글 Oauth2 추가
    
    구글 Oauth2 추가완료
    추가적으로 /user/google/login 접속 시 구글 로그인 주소 반환

---
+++ src/main/java/com/ohh_really/ringdingdong/user/service/UserService.java
@@ -0,0 +1,17 @@
+package com.ohh_really.ringdingdong.user.service;
+
+import com.ohh_really.ringdingdong.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+}
